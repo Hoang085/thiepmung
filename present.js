4398992b572531ef251901c92c85608015542941
Hoang085 <127104291+Hoang085@users.noreply.github.com>
2023-03-07 16:10:44 +0700

    Add files via upload

---
+++ present.js
@@ -0,0 +1,46 @@
+
+var to = 'Bé!';
+var gift_url = 'file:///D:/MVH/Untitled-1.html';
+var gift_image_url = 'file:///C:/Users/asus/Downloads/anh.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
